fb211a0064a0fdb254596bbfa009f49d93801a6d
Jörg Vollmer <kotlincook@gmx.net>
2021-04-06 21:39:41 +0200

    Clean code

--- transporter/src/main/kotlin/transporter/Transporter.kt
+++ transporter/src/main/kotlin/transporter/Transporter.kt
@@ -3,92 +3,79 @@ package transporter
 import org.json.JSONObject
 import redis.clients.jedis.Jedis
 import redis.clients.jedis.exceptions.JedisConnectionException
+import java.lang.Thread.sleep
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
+const val DB_REDIS_NAME = "redis"
+const val DB_REDIS_PASSWORD = "redis_password"
+const val DB_POSTGRES_NAME = "db"
+const val DB_POSTGRES_USER = "postgres"
+const val DB_POSTGRES_PASSWORD = "postgres"
+
 fun main() {
     try {
-        val redis: Jedis = connectToRedis("redis")
-        val dbConn = connectToDB("db")
-        System.err.println("Watching vote queue")
+        val redis = connectToRedis(DB_REDIS_NAME)
+        val dbConn = connectToPostgres(DB_POSTGRES_NAME)
+        println("Watching vote queue")
         while (true) {
-            val voteJSON: String = redis.blpop(0, "votes").get(1)
+            val voteJSON = redis.blpop(0, "votes").get(1)
             val voteData = JSONObject(voteJSON)
-            val voterID: String = voteData.getString("voter_id")
-            val vote: String = voteData.getString("vote")
-            System.err.printf("Processing vote for '%s' by '%s'\n", vote, voterID)
+            val voterID = voteData.getString("voter_id")
+            val vote = voteData.getString("vote")
+            println("Processing vote for '$vote' by '$voterID'")
             updateVote(dbConn, voterID, vote)
         }
-    } catch (e: SQLException) {
+    } catch (e: Exception) {
         e.printStackTrace()
-        System.exit(1)
     }
 }
 
-fun updateVote(dbConn: Connection?, voterID: String?, vote: String?) {
-    val insert = dbConn!!.prepareStatement(
-        "INSERT INTO votes (id, vote) VALUES (?, ?)"
-    )
-    insert.setString(1, voterID)
+fun updateVote(dbConn: Connection, voterId: String, vote: String) {
+    val insert = dbConn.prepareStatement("INSERT INTO votes (id, vote) VALUES (?, ?)")
+    insert.setString(1, voterId)
     insert.setString(2, vote)
     try {
         insert.executeUpdate()
     } catch (e: SQLException) {
-        val update = dbConn.prepareStatement(
-            "UPDATE votes SET vote = ? WHERE id = ?"
-        )
+        val update = dbConn.prepareStatement("UPDATE votes SET vote = ? WHERE id = ?")
         update.setString(1, vote)
-        update.setString(2, voterID)
+        update.setString(2, voterId)
         update.executeUpdate()
     }
 }
 
-fun connectToRedis(host: String?): Jedis {
+fun connectToRedis(host: String): Jedis {
     val conn = Jedis(host)
-    conn.auth("redis_password")
+    conn.auth(DB_REDIS_PASSWORD)
     while (true) {
         try {
             conn.keys("*")
-            break
+            println("Connected to $DB_REDIS_NAME")
+            return conn
         } catch (e: JedisConnectionException) {
-            System.err.println("Waiting for redis")
+            println("Waiting for $DB_REDIS_NAME")
             sleep(1000)
         }
     }
-    System.err.println("Connected to redis")
-    return conn
 }
 
-fun connectToDB(host: String): Connection? {
-    var conn: Connection? = null
-    try {
-        Class.forName("org.postgresql.Driver")
-        val url = "jdbc:postgresql://$host/postgres"
-        while (conn == null) {
-            try {
-                conn = DriverManager.getConnection(url, "postgres", "postgres")
-            } catch (e: SQLException) {
-                System.err.println("Waiting for db")
-                sleep(1000)
-            }
+fun connectToPostgres(host: String): Connection {
+    Class.forName("org.postgresql.Driver")
+    val url = "jdbc:postgresql://$host/postgres"
+    while (true) {
+        try {
+            val conn = DriverManager.getConnection(url, DB_POSTGRES_USER, DB_POSTGRES_PASSWORD)
+            val st = conn.prepareStatement(
+                "CREATE TABLE IF NOT EXISTS votes (id VARCHAR(255) NOT NULL UNIQUE, vote VARCHAR(255) NOT NULL)"
+            )
+            st.executeUpdate()
+            println("Connected to $DB_POSTGRES_NAME")
+            return conn
+        } catch (e: SQLException) {
+            println("Waiting for $DB_POSTGRES_NAME")
+            sleep(1000)
         }
-        val st = conn.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS votes (id VARCHAR(255) NOT NULL UNIQUE, vote VARCHAR(255) NOT NULL)"
-        )
-        st.executeUpdate()
-    } catch (e: ClassNotFoundException) {
-        e.printStackTrace()
-        System.exit(1)
-    }
-    System.err.println("Connected to db")
-    return conn
-}
-
-fun sleep(duration: Long) {
-    try {
-        Thread.sleep(duration)
-    } catch (e: InterruptedException) {
-        System.exit(1)
     }
 }
